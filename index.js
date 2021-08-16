cece8ce4ccbbf06d2b5a95026e0c0166871619a8
Busroni <busroni10@gmail.com>
2021-08-16 20:32:52 +0700

    update v2.0

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-const judul = document.getElementById('judul');
-const ul = document.getElementById('list');
-
-const li = document.createElement('li');
-li.innerText = 'ini baru';
-
-let judulOffset = judul.getBoundingClientRect();
-console.log(judulOffset);
-
-
-setTimeout(()=>{
-    judul.innerText = "ini berubah";
-    judul.removeAttribute('class');
-    ul.appendChild(li);
-    ul.removeChild(ul.firstChild);
-    judulOffset.left = 100;
-},3000)
-
 let i = 1;
 let j = 1;
 let k = 1;
@@ -29,6 +11,11 @@ let score=0;
 const sekarang = document.getElementById('sekarang');
 function multiply(){
 	const multiplied_by =document.getElementsByClassName('span-belt')
+
+	try{
+	let numb=multiplied_by[12].innerHTML
+	} catch (e)
+	{ alert("Sabar tunggu angka sampai tombol ^_^") }
 	let numb=multiplied_by[12].innerHTML
 	if (numb!='-'){
 		now=now*parseFloat(numb);
@@ -38,6 +25,11 @@ function multiply(){
 }
 function plus(){
 	const plus_by =document.getElementsByClassName('span-belt2')
+	
+	try{
+	let numb=plus_by[12].innerHTML
+	} catch (e)
+	{ alert("Sabar tunggu angka sampai tombol ^_^") }
 	let numb=plus_by[12].innerHTML
 	if (numb!='-'){
 		now=now+parseFloat(numb);
@@ -47,7 +39,13 @@ function plus(){
 }
 function minus(){
 	const minus_by =document.getElementsByClassName('span-belt3')
-	let numb=parseInt(minus_by[12].innerHTML)
+	
+	try{
+	let numb=minus_by[12].innerHTML
+	} catch(e) { 
+		alert("Mulai dulu kakak. Kalo sudah sabar, tunggu angka sampai tombol") 
+	}
+	let numb=minus_by[12].innerHTML
 	if (numb!='-'){
 		now=now-parseFloat(numb);
 		console.log(now);
@@ -55,78 +53,84 @@ function minus(){
 	sekarang.innerHTML=now;
 }
 
-const target = document.getElementById('target');
-let angkaTarget = (Math.floor(Math.random()*9)+1)*(Math.floor(Math.random()*9)+1);
-target.innerHTML = angkaTarget ;
-setInterval(()=>{
-	if(now==angkaTarget){
-		score++;
-		const total=document.getElementById('total');
-		total.innerHTML = score ;
-		angkaTarget = (Math.floor(Math.random()*9)+1)*(Math.floor(Math.random()*9)+1);
-		target.innerHTML = angkaTarget;
-	}
-},1000)
+
+function start(){
+
+	const target = document.getElementById('target');
+	let angkaTarget = (Math.floor(Math.random()*9)+1)*(Math.floor(Math.random()*9)+1);
+	target.innerHTML = angkaTarget ;
+	setInterval(()=>{
+		if(now==angkaTarget){
+			score++;
+			const total=document.getElementById('total');
+			total.innerHTML = score ;
+			angkaTarget = (Math.floor(Math.random()*9)+1)*(Math.floor(Math.random()*9)+1);
+			target.innerHTML = angkaTarget;
+		}
+	},1000)
 
 
 
-const belt = document.getElementById('belt');
-console.log(belt);
-setInterval(()=>{
-	const elemen=document.getElementsByTagName('span')
-	temp++;
-},1000)
+	const belt = document.getElementById('belt');
+	console.log(belt);
+	setInterval(()=>{
+		const elemen=document.getElementsByTagName('span')
+		temp++;
+	},1000)
 
-setInterval(()=>{
-    const span = document.createElement('span');
-    if (i==strip){
-        span.innerText = Math.floor(Math.random()*9)+0;
-        i=1;
-        banyak();
-    }else{
-        span.innerText = '-';
-        i++;
-    }
-    belt.appendChild(span);
-    span.classList.add('span-belt')
-	belt.insertBefore(span,belt.childNodes[0]);
-	
-},1000);
+	setInterval(()=>{
+	    const span = document.createElement('span');
+	    if (i==strip){
+	        span.innerText = Math.floor(Math.random()*9)+0;
+	        i=1;
+	        banyak();
+	    }else{
+	        span.innerText = '-';
+	        i++;
+	    }
+	    belt.appendChild(span);
+	    span.classList.add('span-belt')
+		belt.insertBefore(span,belt.childNodes[0]);
+		
+	},1000);
 
-const belt2 = document.getElementById('belt2');
-console.log(belt2);
-setInterval(()=>{
-    const span = document.createElement('span');
-    if (j==strip2){
-        span.innerText = Math.floor(Math.random()*9)+0;
-        j=1;
-        banyak2();
-    }else{
-        span.innerText = '-';
-        j++;
-    }
-    belt2.appendChild(span);
-	span.classList.add('span-belt2')
-    belt2.insertBefore(span,belt2.childNodes[0]);
-},1000);
+	const belt2 = document.getElementById('belt2');
+	console.log(belt2);
+	setInterval(()=>{
+	    const span = document.createElement('span');
+	    if (j==strip2){
+	        span.innerText = Math.floor(Math.random()*9)+0;
+	        j=1;
+	        banyak2();
+	    }else{
+	        span.innerText = '-';
+	        j++;
+	    }
+	    belt2.appendChild(span);
+		span.classList.add('span-belt2')
+	    belt2.insertBefore(span,belt2.childNodes[0]);
+	},1000);
 
-const belt3 = document.getElementById('belt3');
-console.log(belt3);
+	const belt3 = document.getElementById('belt3');
+	console.log(belt3);
 
-setInterval(()=>{
-    const span = document.createElement('span');
-    if (k==strip3){
-        span.innerText = Math.floor(Math.random()*9)+0;
-        k=1;
-        banyak3();
-    }else{
-        span.innerText = '-';
-        k++;
-    }
-    belt3.appendChild(span);
-	span.classList.add('span-belt3')
-	belt3.insertBefore(span,belt3.childNodes[0]);
-},1000);
+	setInterval(()=>{
+	    const span = document.createElement('span');
+	    if (k==strip3){
+	        span.innerText = Math.floor(Math.random()*9)+0;
+	        k=1;
+	        banyak3();
+	    }else{
+	        span.innerText = '-';
+	        k++;
+	    }
+	    belt3.appendChild(span);
+		span.classList.add('span-belt3')
+		belt3.insertBefore(span,belt3.childNodes[0]);
+	},1000);
+
+}
+
 
 function banyak(){
     strip = Math.floor(Math.random()*6)+2;
